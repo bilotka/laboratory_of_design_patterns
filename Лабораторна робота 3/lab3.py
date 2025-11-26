af3eb0dbdaeaff71cc2756c09e61a35d729bd981
Katria <moisei.kateryna@student.uzhnu.edu.ua>
2025-11-26 03:43:18 +0200

    Лабораторна робота 3.2

--- Лабораторна робота 3/lab3.py
+++ Лабораторна робота 3/lab3.py
@@ -3,70 +3,81 @@
 # 2 послідовних запитів для замовлення, а Uber приймає # запит із 3 параметрами (адреса початку, адреса кінця та час замовлення).Наприклад, для поїздки із вул. Університетська 
 # 14 на пл. # Народна 3 Uklon прийме запит ("пл. Народна 3", "вул. Університетська 14") Bolt приймає 2 запити ("begin", "вул. Університетська 14") # та ("end", "пл. Народна 3 "),
 #  а Uber приймає запит ("вул. Університетська 14", "пл. Народна 3", 9:30 21.09.2022)
+from typing import Protocol
 from datetime import datetime
 
+class ITaxi(Protocol):
+    def order_trip(self, start: str, end: str) -> None:
+        ...
+
 class Uklon:
-    def make_order(self, end, start):
+    def make_order(self, end: str, start: str):
         print(f"[Uklon] Замовлення створено: з {start} до {end}")
 
+
 class Bolt:
-    def request(self, point_type, address):
-        print(f"[Bolt] Запит: {point_type} -> {address}")
+    def request(self, point_type: str, address: str):
+        print(f"[Bolt] Запит: {point_type} → {address}")
+
 
 class Uber:
-    def order(self, start, end, time):
-        print(f"[Uber] Замовлення: {start} -> {end} о {time}")
-
+    def order(self, start: str, end: str, time: str):
+        print(f"[Uber] Замовлення: {start} → {end} о {time}")
 
 class UklonAdapter:
-    def __init__(self, service):
+    def __init__(self, service: Uklon):
         self.service = service
 
-    def order_trip(self, start, end):
+    def order_trip(self, start: str, end: str):
         self.service.make_order(end, start)
 
 
 class BoltAdapter:
-    def __init__(self, service):
+    def __init__(self, service: Bolt):
         self.service = service
 
-    def order_trip(self, start, end):
+    def order_trip(self, start: str, end: str):
         self.service.request("begin", start)
         self.service.request("end", end)
 
 
 class UberAdapter:
-    def __init__(self, service):
+    def __init__(self, service: Uber):
         self.service = service
 
-    def order_trip(self, start, end):
+    def order_trip(self, start: str, end: str):
         now = datetime.now().strftime("%H:%M %d.%m.%Y")
         self.service.order(start, end, now)
 
 
 class TaxiIntegrator:
     def __init__(self):
-        self.services = []
+        self.services: list[ITaxi] = []
 
     def add_service(self, service):
-        self.services.append(service)
 
-    def order_taxi(self, start, end):
-        print(f"\n Замовлення таксі з {start} до {end}")
+        if isinstance(service, Uklon):
+            self.services.append(UklonAdapter(service))
+
+        elif isinstance(service, Bolt):
+            self.services.append(BoltAdapter(service))
+
+        elif isinstance(service, Uber):
+            self.services.append(UberAdapter(service))
+
+    def order_taxi(self, start: str, end: str):
+        print(f"\nЗамовлення таксі з {start} до {end}")
         for service in self.services:
             service.order_trip(start, end)
 
 
 # Приклад виконання
 def test_adapter() -> None: 
-    uklon = UklonAdapter(Uklon()) 
-    bolt = BoltAdapter(Bolt()) 
-    uber = UberAdapter(Uber()) 
-    integrator = TaxiIntegrator() 
-    integrator.add_service(uklon) 
-    integrator.add_service(bolt) 
-    integrator.add_service(uber) 
-    integrator.order_taxi("вул. Університетська 14", "пл. Народна 3") 
+    integrator.add_service(Uklon())
+    integrator.add_service(Bolt())
+    integrator.add_service(Uber())
+
+    integrator.order_taxi("вул. Університетська 14", "пл. Народна 3")
     
 if __name__ == "__main__": 
     test_adapter()
